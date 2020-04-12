b4e930cd127fc81480d3b603c5849e1aabbe039a
tirth97 <patel.tir@husky.neu.edu>
2020-04-11 22:07:12 -0400

    example of spring expression language

--- Conference-Java/src/main/java/Application.java
+++ Conference-Java/src/main/java/Application.java
@@ -18,5 +18,7 @@ public class Application {
     SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
     System.out.println(service.findAll().get(0).getFirstName());
+
+    System.out.println(service.findAll().get(0).getSeedNum());
   }
 }
